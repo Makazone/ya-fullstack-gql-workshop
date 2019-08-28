3714ff9fa0c8be245c10da310378d1184db2e7e8
Makar Stetsenko <makazone@gmail.com>
2019-08-29 01:42:49 +0300

    build(api): scaffolding

---
+++ api/README.md
@@ -0,0 +1,7 @@
+# To start
+
+1. yarn
+2. yarn setup-envs
+3. Edit .env file
+4. NODE_ENV=development yarn build
+5. yarn start
---
+++ api/src/main.ts
@@ -0,0 +1,5 @@
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => console.log("Module disposed. "));
+}
