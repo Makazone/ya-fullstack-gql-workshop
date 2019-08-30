fcc16f7c729bfc79b1635dd0b223c27e5be38ae2
Makar Stetsenko <makazone@gmail.com>
2019-08-30 17:04:05 +0300

    allow cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,13 @@ import { ApolloServer } from "apollo-server";
 import resolvers from "./resolvers";
 import typeDefs from "./schema";
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+  cors: {
+    origin: "*"
+  },
+  resolvers,
+  typeDefs
+});
 
 server.listen().then(({ url }) => console.log(`Server ready at ${url}. `));
 
