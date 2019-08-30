2dd5e4a53ea9cc8b7c384309a2f5db0fb2f4b812
Makar Stetsenko <makazone@gmail.com>
2019-08-30 17:06:01 +0300

    apollo init

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Movie from './Movie'
 import { Menu } from 'semantic-ui-react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 const App: React.FC = () => {
   const movieId = '45e8f29f25fe31a59be88301baa5caa1'
 
   return (
-    <>
+    <ApolloProvider client={client}>
       <Menu>
         <Menu.Item>
           Ya-Movies
           </Menu.Item>
       </Menu>
       <Movie movieId={movieId} />
-    </>
+    </ApolloProvider>
   );
 }
 
